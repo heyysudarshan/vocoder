bcaf20b8b2de022e8149e7139e0344854a5e628c
= <=>
2025-08-11 14:19:47 +0530

    Define android-app module's configuration information from the version catalog

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
 
 }
 
+group = libs.versions.androidApp.group.get()
+version = libs.versions.androidApp.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/AndroidApp"))
