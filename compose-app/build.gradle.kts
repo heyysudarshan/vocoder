f3502416e6f1296fbeb1dcb163ae7a6b7b69f1d9
= <=>
2025-08-11 16:07:26 +0530

    Define compose-app module's configuration information from the version catalog

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
 
 }
 
+group = libs.versions.composeApp.group.get()
+version = libs.versions.composeApp.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/ComposeApp"))
