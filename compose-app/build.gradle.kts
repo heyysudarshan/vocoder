3296a7c6a9dccb898ed76e8d2fabb24ccfaf6ef4
= <=>
2025-08-11 16:13:04 +0530

    Define targets in compose-app module for Kotlin Multiplatform

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -2,6 +2,23 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
 
+kotlin {
+    androidTarget()
+
+    jvm("desktop")
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+}
+
 group = libs.versions.composeApp.group.get()
 version = libs.versions.composeApp.version.get()
 
