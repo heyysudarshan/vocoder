dc2dbd3e8cc0fb4b2100741f2d3cf8fd37f85ce8
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-12 12:40:27 +0530

    Enable edge-to-edge mode in MainActivity

--- android-app/src/main/kotlin/com/sudarshanmhasrup/vocoder/MainActivity.kt
+++ android-app/src/main/kotlin/com/sudarshanmhasrup/vocoder/MainActivity.kt
@@ -3,12 +3,14 @@ package com.sudarshanmhasrup.vocoder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity: ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        enableEdgeToEdge()
         setContent {
 
         }
